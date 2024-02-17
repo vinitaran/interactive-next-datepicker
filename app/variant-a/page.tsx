e23f3bdc8ae86c87e5bc12362ee9cf8d4676b9cf
Vinita <vinita.rane@freezecarbon.com>
2024-02-17 09:10:41 +0100

    a nd b basic done

---
+++ app/variant-a/page.tsx
@@ -0,0 +1,173 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const page = () => {
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+  const [keyCount, setKeyCount] = useState(0);
+  const [backspaceCount, setBackspaceCount] = useState(0);
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [timerInterval, setTimerInterval] = useState<NodeJS.Timeout | null>(
+    null
+  );
+  const [timeTaken, setTimeTaken] = useState<number | null>(null);
+  const [averageTypingSpeed, setAverageTypingSpeed] = useState(0);
+
+  useEffect(() => {
+    setStartTime(Date.now()); // Record the start time when the component mounts
+  }, []);
+  const startTimer = () => {
+    setStartTime(Date.now());
+  };
+
+  const stopTimer = () => {
+    if (timerInterval) clearInterval(timerInterval);
+    setElapsedTime(Date.now() - (startTime || 0));
+  };
+
+  const updateTime = () => {
+    const currentTime = Date.now();
+    const elapsedTimeInSeconds = (currentTime - (startTime || 0)) / 1000;
+    setElapsedTime(elapsedTimeInSeconds);
+  };
+
+  const handleInputClick = () => {
+    startTimer();
+  };
+
+  const handleSubmit = () => {
+    stopTimer();
+    const currentTime = Date.now();
+    const timeTakenInSeconds = (currentTime - (startTime || 0)) / 1000;
+
+    setTimeTaken(timeTakenInSeconds); // Update the timeTaken state
+    if (timeTakenInSeconds && keyCount > 0) {
+      const speed = (keyCount + backspaceCount) / timeTakenInSeconds;
+      setAverageTypingSpeed(parseFloat(speed.toFixed(2)));
+    }
+
+    const date = `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+    console.log(date); // Dispatch or handle the date as needed
+
+    const notificationModal = document.getElementById("notificationModal");
+    if (notificationModal) {
+      const notificationContent = notificationModal.querySelector(
+        ".notification-content"
+      );
+      if (notificationContent instanceof HTMLElement) {
+        notificationContent.style.display = "block";
+      }
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const allowedKeys = [46, 8, 9, 27, 13, 110, 190];
+    const ctrlKeys = [65, 67, 86, 88];
+    const isCtrlPressed = event.ctrlKey || event.metaKey;
+    const isSpecialKey =
+      allowedKeys.includes(event.keyCode) ||
+      (ctrlKeys.includes(event.keyCode) && isCtrlPressed);
+    const isNavigationKey = event.keyCode >= 35 && event.keyCode <= 40;
+    const isNumericInput =
+      (!event.shiftKey && event.keyCode >= 48 && event.keyCode <= 57) ||
+      (event.keyCode >= 96 && event.keyCode <= 105);
+
+    if (event.keyCode === 8) setBackspaceCount(backspaceCount + 1);
+
+    if (!(isSpecialKey || isNavigationKey || isNumericInput)) {
+      event.preventDefault();
+    } else {
+      setKeyCount(keyCount + 1);
+    }
+  };
+
+  const navigateToVariantB = () => {
+    console.log("Navigating to Variant B...");
+    window.location.href = "/variant-b"; // Navigate to Variant B page
+    console.log("Navigation complete.");
+  };
+
+  return (
+    <div className="text-center mt-5">
+      <h1>Enter your Birthday</h1>
+
+      <div className="form-group date-picker">
+        <div className="form-group">
+          <label htmlFor="day">Day</label>
+          <input
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+            id="day"
+            className="date-picker--day"
+            type="number"
+            min="1"
+            max="31"
+            placeholder="dd"
+            maxLength={2}
+            onClick={handleInputClick}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="month">Month</label>
+          <input
+            value={month}
+            onChange={(e) => setMonth(e.target.value)}
+            id="month"
+            className="date-picker--month"
+            type="number"
+            min="1"
+            max="12"
+            placeholder="mm"
+            maxLength={2}
+            onClick={handleInputClick}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="year">Year</label>
+          <input
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+            id="year"
+            className="date-picker--year"
+            type="number"
+            placeholder="yyyy"
+            maxLength={4}
+            onClick={handleInputClick}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+      </div>
+
+      <div className="notification-modal" id="notificationModal">
+        <div className="notification-content">
+          <h2>Take a Screenshot!</h2>
+          <p>
+            Please take a screenshot of your submission. Click below to proceed
+            to Variant B.
+          </p>
+          <button className="btn btn-primary" onClick={navigateToVariantB}>
+            Proceed to Variant B
+          </button>
+        </div>
+      </div>
+
+      <div>Key Count: {keyCount}</div>
+      <div>Backspace Count: {backspaceCount}</div>
+      <div>Time Taken: {(timeTaken || 0 / 1000).toFixed(2)} seconds</div>
+      <div>
+        Average Typing Speed: {averageTypingSpeed.toFixed(2)} keys per second
+      </div>
+
+      <button className="btn btn-primary mt-3" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/variant-b/page.tsx
@@ -0,0 +1,143 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const page = () => {
+  const [dateInput, setDateInput] = useState("");
+  const [keyCount, setKeyCount] = useState(0);
+  const [backspaceCount, setBackspaceCount] = useState(0);
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(0);
+  const [timeTaken, setTimeTaken] = useState(0);
+  const [averageSpeed, setAverageSpeed] = useState(0);
+  const [isBackspacePressed, setIsBackspacePressed] = useState(false);
+  const [isTimeStarted, setIsTimeStarted] = useState(false);
+  const [isValidDate, setIsValidDate] = useState(false);
+  const [errorSummary, setErrorSummary] = useState("");
+
+  useEffect(() => {
+    // Start the timer when the component mounts
+    startTimer();
+  }, []);
+
+  const navigateToVariantB = () => {
+    console.log("Navigating to Variant B...");
+    window.location.href =
+      "https://docs.google.com/forms/d/e/1FAIpQLSfN5KSoGmuhVcXFXDH3oUEPKHdj0gL-akc3NcKcvvXsCcY0-w/viewform";
+    console.log("Navigation complete.");
+  };
+
+  const startTimer = () => {
+    setStartTime(Date.now());
+    setIsTimeStarted(true);
+  };
+
+  const stopTimer = () => {
+    setEndTime(Date.now());
+    setIsTimeStarted(false);
+  };
+
+  const autoFormatDate = (input: string) => {
+    let preFormattedInput = input;
+
+    if (input && !isBackspacePressed) {
+      preFormattedInput = preFormattedInput.replaceAll("/", "");
+
+      if (preFormattedInput.length > 2) {
+        preFormattedInput =
+          preFormattedInput.slice(0, 2) +
+          "/" +
+          preFormattedInput.slice(2, 4) +
+          "/" +
+          preFormattedInput.slice(4, 8);
+      } else if (preFormattedInput.length > 4) {
+        preFormattedInput =
+          preFormattedInput.slice(0, 4) + "/" + preFormattedInput.slice(4, 8);
+      }
+    }
+
+    setDateInput(preFormattedInput);
+  };
+
+  const onDateInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    autoFormatDate(value);
+    if (!isTimeStarted) startTimer();
+  };
+
+  const validateDate = () => {
+    const modal = document.getElementById("notificationModal");
+    if (modal) modal.style.display = "block";
+    if (dateInput) {
+      const [day, month, year] = dateInput.split("/");
+      const formattedDate = new Date(`${year}-${month}-${day}`);
+      setIsValidDate(
+        !isNaN(formattedDate.getDate()) &&
+          formattedDate.getFullYear() == parseInt(year)
+      );
+    } else {
+      setIsValidDate(false);
+      setErrorSummary("");
+    }
+    // Stop the timer when validating
+    stopTimer();
+    calculateSpeed();
+  };
+
+  const keyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const keyCode = event.keyCode || event.which;
+    setIsBackspacePressed(keyCode === 8);
+    if (keyCode === 8) setBackspaceCount(backspaceCount + 1);
+    setKeyCount(keyCount + 1);
+  };
+  const calculateSpeed = () => {
+    const timeTakenInSeconds = Math.abs((endTime - startTime) / 1000);
+
+    setTimeTaken(timeTakenInSeconds);
+
+    // Check if timeTakenInSeconds and keyCount are both greater than 0
+    if (timeTakenInSeconds > 0 && keyCount > 0) {
+      const speed = keyCount / timeTakenInSeconds; // keys per second
+      setAverageSpeed(parseFloat(speed.toFixed(2)));
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Enter your Birthday</h1>
+      <input
+        id="dateInput"
+        className="date-input"
+        type="text"
+        placeholder="DD/MM/YYYY"
+        value={dateInput}
+        onChange={onDateInputChange}
+        onKeyDown={keyDown}
+      />
+
+      <div className={`error-summary ${isValidDate ? "" : "error"}`}>
+        {errorSummary}
+      </div>
+      <div>Key Count: {keyCount}</div>
+      <div>Backspace Count: {backspaceCount}</div>
+      <div>Time Taken: {timeTaken} milliseconds</div>
+      <div>Average Speed: {averageSpeed.toFixed(2)} keys per second</div>
+      <button className="calculate" onClick={validateDate}>
+        Validate
+      </button>
+
+      <div className="notification-modal" id="notificationModal">
+        <div className="notification-content">
+          <h2>Take a Screenshot!</h2>
+          <p>
+            Please take a screenshot of your submission. Click below to proceed.
+          </p>
+          <button className="btn btn-primary" onClick={navigateToVariantB}>
+            Proceed
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
